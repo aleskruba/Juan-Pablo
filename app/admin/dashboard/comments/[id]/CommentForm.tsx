59deb72fe960937ab46546c73bf450613173cb3a
Ales <aleskruba@seznam.cz>
2024-01-01 16:21:55 +0100

    create comments functionalities update

--- app/admin/dashboard/comments/[id]/CommentForm.tsx
+++ app/admin/dashboard/comments/[id]/CommentForm.tsx
@@ -48,6 +48,7 @@ interface Comment {
           const data = await response.json();
     
           if (data.message === 'success'){
+            await fetchComments()
             toast.success('Comment updated successfully')
             router.push('/admin/dashboard/comments')
             setIsLoadingUpdate(false)
