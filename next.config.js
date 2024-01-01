66511622d6ce4f8192cb8c1f7eeebd26eadc2147
Ales <aleskruba@seznam.cz>
2024-01-01 18:27:38 +0100

    next config II

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
- images: {
   disableManifest: true,
-  cacheBust: true,
-   domains: [
-         'lh3.googleusercontent.com'
-   ]
- } 
+
+      images: {
+        
+        domains: [
+              'lh3.googleusercontent.com'
+        ]
+      } 
 }
 
 module.exports = nextConfig
